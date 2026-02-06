fdd50b6d907b84dd4a9d3cd6e4415bfe4e88375e
shiva1338 <shivvvvaaaaa@gmail.com>
2026-02-06 14:58:26 +0530

    Update bronze_ddl.sql

--- SCRIPTS /bronze_ddl.sql
+++ SCRIPTS /bronze_ddl.sql
@@ -10,14 +10,14 @@ if object_id ('bronze.crm_cust_info','U') is not null
    create table bronze customer info
    ==================================*/
 create table bronze.crm_cust_info (
-        			cst_id int ,
-        			cst_key nvarchar(50),
-        			cst_firstname nvarchar(50),
-        			cst_lastname nvarchar(50),
-        			cst_marital_status nvarchar(50),
-        			cst_gndr nvarchar(50),
-        			cst_create_date date
-               )
+			cst_id int ,
+			cst_key nvarchar(50),
+			cst_firstname nvarchar(50),
+			cst_lastname nvarchar(50),
+			cst_marital_status nvarchar(50),
+			cst_gndr nvarchar(50),
+			cst_create_date date
+            )
 
 if object_id ('bronze.crm_prd_info','U') is not null
  drop table bronze.crm_prd_info
@@ -26,13 +26,13 @@ if object_id ('bronze.crm_prd_info','U') is not null
 creating table bronze productinfo
 ========================================*/
 create table bronze.crm_prd_info (
-      			 prd_id int ,
-      			 prd_key nvarchar(50),
-      			 prd_name nvarchar(50),
-      			 prd_cost int ,
-      			 prd_line nvarchar(50),
-      			 prd_start_date datetime,
-      			 prd_end_date datetime
+			 prd_id int ,
+			 prd_key nvarchar(50),
+			 prd_name nvarchar(50),
+			 prd_cost int ,
+			 prd_line nvarchar (50),
+			 prd_start_date datetime,
+			 prd_end_date datetime
              )
 
 if object_id ('bronze.crm_sales','U') is not null
@@ -42,17 +42,17 @@ go
 ---- creating table bronze salesinfo 
 ==========================================*/
 create table bronze.crm_sales (
-        			sls_ord_num nvarchar (50),
-        			sls_prd_key nvarchar(50),
-        			sls_cust_id int,
-        			sls_order_date date,
-        			sls_ship_date date,
-        			sls_due_date date,
-        			sls_sales int,
-        			sls_quantity int,
-        			sls_price int
-        			)
-          
+			sls_ord_num nvarchar (50),
+			sls_prd_key nvarchar(50),
+			sls_cust_id int,
+			sls_order_date int,
+			sls_ship_date int,
+			sls_due_date int,
+			sls_sales int,
+			sls_quantity int,
+			sls_price int
+			)
+
 if object_id ('bronze.erp_cust_ss','U') is not null
 drop table bronze.erp_cust_ss
 go 
@@ -60,10 +60,10 @@ go
 ---- creating table bronze cust_ss
 ===========================================*/
 create table bronze.erp_cust_ss (
-      				cus_id nvarchar(50),
-      				cus_birthdate date,
-      				cus_gender nvarchar(50)
-      				)
+				cus_id nvarchar(50),
+				cus_birthdate date,
+				cus_gender nvarchar(50)
+				)
 
 if object_id ('bronze.erp_log','U') is not null
 drop table bronze.erp_log
@@ -73,9 +73,9 @@ go
 ==========================================*/
 
 create table bronze.erp_log (
-      				c_id nvarchar(50),
-      				c_country nvarchar(50)
-      				)
+				c_id nvarchar(50),
+				c_country nvarchar(50)
+				)
 
 if object_id ('bronze.erp_prd_cat','U') is not null
 drop table bronze.erp_prd_cat
@@ -84,11 +84,13 @@ go
 ----- creating table bronze product category
 ==================================================*/
 create table bronze.erp_prd_cat(
-        			prdd_id nvarchar(50),
-        			prdd_cat nvarchar(50),
-        			prdd_subcat nvarchar(50),
-        			prdd_maintaince nvarchar(50)
-        			)
+			prdd_id nvarchar(50),
+			prdd_cat nvarchar(50),
+			prdd_subcat nvarchar(50),
+			prdd_maintaince nvarchar(50)
+			)
+
+
 
 
 
